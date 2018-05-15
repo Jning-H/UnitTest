e4734f6356c77189a12433811c67c79438366e7f
Jning <83542047@qq.com>
2018-05-15 18:16:27 +0800

    0

--- app/src/main/java/com/jning/unittest/MainActivity.java
+++ app/src/main/java/com/jning/unittest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jning.unittest;
 
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,
+                BuildConfig.BUILD_TYPE+"\n"+
+                        BuildConfig.DEBUG+"\n"+
+                        BuildConfig.FLAVOR+"\n"
+                ,Toast.LENGTH_LONG).show();
     }
 }
